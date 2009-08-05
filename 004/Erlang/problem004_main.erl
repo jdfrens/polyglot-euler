7995613f107de5d12348670d6219ecad700dc38a
Jeremy D. Frens <jdfrens@gmail.com>
2009-08-05 13:08:45 -0400

    removed an unncessary TODO

--- 004/Erlang/problem004_main.erl
+++ 004/Erlang/problem004_main.erl
@@ -3,7 +3,6 @@
 -export([main/1]).
 -import(problem004, [findPalindromeProduct/1]).
 
-% TODO: write Erlang CLI
 main([A]) -> 
 	I = list_to_integer(atom_to_list(A)), 
 	S = problem004:findPalindromeProduct(I), 
