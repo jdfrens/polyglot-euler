3548c09624d6da1161b7fcb42609ec271bcea1a8
Jeremy D. Frens <jdfrens@gmail.com>
2009-08-17 09:44:21 -0400

    wrote Haskell solution for problem 6; deleted Prolog code

--- 006/Haskell/Problem006.hs
+++ 006/Haskell/Problem006.hs
@@ -1,9 +1,8 @@
 module Problem006 (diff_squares_sums, sum_of_squares, square_of_sum) where
 
 diff_squares_sums :: Integer -> Integer
-diff_squares_sums n = 3
+diff_squares_sums n = (square_of_sum n) - (sum_of_squares n)
 
-sum_of_squares n = 3
+sum_of_squares n = n * (n + 1) * (2 * n + 1) `div` 6
 
-square_of_sum n = 3
-
+square_of_sum n = (n * (n + 1) `div` 2)^2
--- 006/Prolog/problem006.pl
+++
@@ -1,6 +0,0 @@
-diff_squares_sums(_, 3).
-
-sum_of_squares(_, 3).
-
-square_of_sum(_, 3).
-
