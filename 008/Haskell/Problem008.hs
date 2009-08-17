3f6a32275481401b164959401dd2c153d29a56b8
Jeremy D. Frens <jdfrens@gmail.com>
2009-08-17 19:58:34 -0400

    deleting an Erlang crash; solved problem 8 with Haskell

--- 008/Haskell/Problem008.hs
+++ 008/Haskell/Problem008.hs
@@ -1,5 +1,9 @@
 module Problem008 (largestFiveDigitProduct) where
 
 largestFiveDigitProduct :: Integer -> Integer
-largestFiveDigitProduct n = 3
-
+largestFiveDigitProduct n = lfdp (listify n []) 0
+  where
+    lfdp list@(a:b:c:d:e:rest) current = lfdp (tail list) (max current (a*b*c*d*e))
+    lfdp _ current = current
+    listify 0 list = list
+    listify n list = listify (n `div` 10) (n `mod` 10 : list)
