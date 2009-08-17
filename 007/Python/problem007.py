9958972bc9768c4f82be256d8fb2eea608275a6a
Jeremy D. Frens <jdfrens@gmail.com>
2009-08-17 14:26:53 -0400

    wrote Ruby solution for problem 7; had to eliminate tail recursion in Python and Ruby solutions

--- 007/Python/problem007.py
+++ 007/Python/problem007.py
@@ -5,22 +5,24 @@ def nth_prime(n):
         return nth_prime_after(n-1, 3, 1 * 2)
         
 def nth_prime_after(n, p, product_of_primes):
-    if n == 1:
-        return p
-    else:
-        return nth_prime_after(n-1, next_prime(p+2, product_of_primes), product_of_primes * p)
+    while n > 1:
+        old_p = p
+        p = next_prime(p+2, product_of_primes)
+        product_of_primes *= old_p
+        n = n - 1
+    return p
 
 def next_prime(n, product_of_primes):
-    if is_prime(n, product_of_primes):
-        return n
-    else:
-        return next_prime(n+2, product_of_primes)
+    while not is_prime(n, product_of_primes):
+        n = n + 2
+    return n
 
 def is_prime(n, product_of_primes):
     return gcd(product_of_primes, n) == 1
 
 def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a % b)
+    while b != 0:
+        old_a = a
+        a = b
+        b = old_a % b
+    return a
--- 007/Ruby/problem007.rb
+++ 007/Ruby/problem007.rb
@@ -1,7 +1,44 @@
 class Integer
 
   def nth_prime
-    3
+    if self == 1
+      2
+    else
+      (self-1).nth_prime_after(3, 1 * 2)
+    end
+  end
+
+  def nth_prime_after(p, product_of_primes)
+    n = self
+    while n > 1
+      old_p = p
+      p = (p+2).next_prime(product_of_primes)
+      product_of_primes *= old_p
+      n = n - 1
+    end
+    p
+  end
+
+  def next_prime(product_of_primes)
+    n = self
+    while not n.is_prime(product_of_primes) do
+      n = n + 2
+    end
+    n
+  end
+  
+  def is_prime(product_of_primes)
+    gcd(product_of_primes) == 1
+  end
+
+  def gcd(b)
+    a = self
+    while b != 0
+      prev_a = a
+      a = b
+      b = prev_a.remainder(b)
+    end
+    a
   end
 
 end
