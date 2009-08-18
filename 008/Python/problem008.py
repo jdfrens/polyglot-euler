33a1aa80705c3b079e48d4b1412f305326c9dcf6
Jeremy D. Frens <jdfrens@gmail.com>
2009-08-17 20:25:47 -0400

    solved problem 8 in Python

--- 008/Python/problem008.py
+++ 008/Python/problem008.py
@@ -1,3 +1,18 @@
 def largest_five_digit_product(n):
-    return 3
+    return lfdp(listify(n), 0)
     
+def lfdp(list, current):
+    if len(list) < 5:
+        return current
+    else:
+        return lfdp(list[1:], max(current, product5(list)))
+        
+def product5(list):
+    return list[0] * list[1] * list[2] * list[3] * list[4]
+
+def listify(n):
+    list = []
+    while n > 0:
+        list = [n % 10] + list
+        n /= 10
+    return list
