180a945a5415c58331ca16e3ae93a08d8bffdec7
Jeremy D. Frens <jdfrens@gmail.com>
2009-08-17 20:47:09 -0400

    problem 8 solved in Ruby

--- 008/Ruby/problem008.rb
+++ 008/Ruby/problem008.rb
@@ -1,7 +1,29 @@
 class Integer
 
   def largest_five_digit_product
-    3
+    lfdp(self.listify, 0)
+  end
+  
+  def lfdp(list, current)
+    if list.length < 5
+      current
+    else
+      lfdp(list[1, list.length], [current, product5(list)].max)
+    end
   end
 
+  def product5(list)
+    list[0] * list[1] * list[2] * list[3] * list[4]
+  end
+
+  def listify
+    n = self
+    list = []
+    while n > 0 do
+      list = [n % 10] + list
+      n /= 10
+    end
+    list
+  end
+  
 end
