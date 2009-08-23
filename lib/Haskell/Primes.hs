84ece6915b3b2478311d3f69483fb1701bbafd10
Jeremy D. Frens <jdfrens@gmail.com>
2009-08-23 19:30:00 -0400

    small whitespace tweaking

--- lib/Haskell/Primes.hs
+++ lib/Haskell/Primes.hs
@@ -2,7 +2,7 @@ module Primes (primes, primeFactors) where
 
 primes :: [Integer]
 primes = sieve [2..]
-  where sieve (p:xs) = p : sieve [x | x<-xs, x `mod` p /= 0]
+  where sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
 primeFactors 1 = []
 primeFactors n = go n primes
