ed7023a5b1ad085328408479bd14e31b5d23c46e
Jeremy D. Frens <jdfrens@gmail.com>
2009-09-01 18:39:53 -0500

    made Python code for prime generation iterative

--- lib/Python/primes.py
+++ lib/Python/primes.py
@@ -1,16 +1,13 @@
 from functional import foldl
 
 def primes_up_to(n):
-    return primes_up_to_acc(n, 2, [])
-    
-def primes_up_to_acc(n, i, primes):
-    if i > n:
-        return primes
-    else:
+    primes = []
+    i = 2
+    while i <= n:
         if is_prime(i, primes):
             primes.append(i)
-        primes_up_to_acc(n, i+1, primes)
-        return primes
+        i = i + 1
+    return primes
 
 def is_prime(i, primes):
     return not foldl(lambda a, b: a or b, False, [i % p == 0 for p in primes])
