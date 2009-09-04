9a3c7d3382a3fa99e44bb68caa38280fd00474a2
Jeremy D. Frens <jdfrens@gmail.com>
2009-09-04 17:18:07 -0500

    can factor a number in Python

--- lib/Python/primes.py
+++ lib/Python/primes.py
@@ -1,4 +1,5 @@
 from functional import foldl
+import math
 
 def primes_up_to(n):
     primes = []
@@ -11,3 +12,10 @@ def primes_up_to(n):
 
 def is_prime(i, primes):
     return not foldl(lambda a, b: a or b, False, [i % p == 0 for p in primes])
+
+def factors(n):
+    if n == 1:
+        return [1]
+    LowFactors = filter(lambda a: n % a == 0, range(1, math.sqrt(n)+1))
+    HighFactors = map(lambda a: n / a, LowFactors)
+    return LowFactors + HighFactors[::-1]
--- lib/Python/primes_tests.py
+++ lib/Python/primes_tests.py
@@ -18,4 +18,13 @@ class PrimesTests(unittest.TestCase):
             [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 
             primes.primes_up_to(30))
 
+    def test_factors(self):
+      self.assertEqual([1], primes.factors(1))
+      self.assertEqual([1, 3], primes.factors(3))
+      self.assertEqual([1, 2, 3, 6], primes.factors(6))
+      self.assertEqual([1, 2, 5, 10], primes.factors(10))
+      self.assertEqual([1, 3, 5, 15], primes.factors(15))
+      self.assertEqual([1, 3, 7, 21], primes.factors(21))
+      self.assertEqual([1, 2, 4, 7, 14, 28], primes.factors(28))
+
 unittest.main()
