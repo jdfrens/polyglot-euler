000f9cd4d6dcb33fcbc862545851d07c4bca003b
Jeremy D. Frens <jdfrens@gmail.com>
2009-09-04 17:33:54 -0500

    integers can be factored in Ruby

--- lib/Ruby/primes.rb
+++ lib/Ruby/primes.rb
@@ -3,6 +3,16 @@ class Integer
     @@generator ||= PrimeGenerator.new
     @@generator.upto(options[:less_than])
   end
+  
+  def factors()
+    if self == 1
+      [1]
+    else
+      low_factors = (1..Math.sqrt(self).round).to_a.select { |n| self % n == 0 }
+      high_factors = low_factors.map { |n| self / n }.reverse
+      low_factors + high_factors
+    end
+  end
 end
 
 class PrimeGenerator
--- lib/Ruby/primes_spec.rb
+++ lib/Ruby/primes_spec.rb
@@ -1,12 +1,22 @@
 require 'primes'
 require 'spec'
 
-describe "generating primes" do
+describe Integer do
   it "should generate primes" do
     Integer.primes(:less_than => 2).should == [2]
     Integer.primes(:less_than => 3).should == [2, 3]
     Integer.primes(:less_than => 9).should == [2, 3, 5, 7]
     Integer.primes(:less_than => 30).should == 
       [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+  end
+  
+  it "should have factors" do
+    1.factors.should == [1]
+    3.factors.should == [1, 3]
+    6.factors.should == [1, 2, 3, 6]
+    10.factors.should == [1, 2, 5, 10]
+    15.factors.should == [1, 3, 5, 15]
+    21.factors.should == [1, 3, 7, 21]
+    28.factors.should == [1, 2, 4, 7, 14, 28]
   end
 end
