eb716820dbd9d2b2cf0d58833c2497f90961b4f7
Jeremy D. Frens <jdfrens@gmail.com>
2009-09-04 19:37:33 -0500

    problem 12 solved in Ruby

--- 012/Ruby/problem012.rb
+++ 012/Ruby/problem012.rb
@@ -1,9 +1,15 @@
 $LOAD_PATH << "../../lib/Ruby"
 
+require 'numbers'
+
 class Integer
 
   def problem012
-    3
+    i = 1
+    while i.triangle_number.factors.length < self do
+      i = i + 1
+    end
+    i.triangle_number
   end
 
 end
