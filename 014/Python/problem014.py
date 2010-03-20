8323953bef94a390c972a0721a40f2ed8f19f898
Jeremy D. Frens <jdfrens@gmail.com>
2010-03-20 12:15:29 -0500

    wrote Python solution for Problem 14

--- 014/Python/problem014.py
+++ 014/Python/problem014.py
@@ -1,6 +1,19 @@
 def collatz(n):
-    return 3
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        return collatz(n / 2) + 1
+    else:
+        return collatz(3 * n + 1) + 1
     
 def maximum_collatz_under(n):
-    return 3
-    
+    if n == 1:
+        return 1
+    maximum = 0
+    maxIndex = 0
+    for i in range(1, n+1):
+        collatzOfI = collatz(i)
+        if collatzOfI > maximum:
+            maximum = collatzOfI
+            maxIndex = i
+    return maxIndex    
