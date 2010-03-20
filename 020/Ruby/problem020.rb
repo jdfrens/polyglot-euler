a55fe2d1a1b2b995d7d58368d9de44e9928965c2
Jeremy D. Frens <jdfrens@gmail.com>
2010-03-20 14:58:41 -0500

    solved Problem 20 in Ruby

--- 020/Ruby/problem020.rb
+++ 020/Ruby/problem020.rb
@@ -1,13 +1,19 @@
 $LOAD_PATH << "../../lib/Ruby"
 
+require 'numbers'
+
 class Integer
 
   def sum_of_digits_of_factorial
-    3
+    self.factorial.sum_of_digits
   end
 
   def sum_of_digits
-    3
+    if self < 10
+      self
+    else
+      self % 10 + (self / 10).sum_of_digits
+    end
   end
 
 end
